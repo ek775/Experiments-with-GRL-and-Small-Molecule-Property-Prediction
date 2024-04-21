dee38b4fc301ce7143f3f53555f4cbfbd921d6cd
ek775 <eliotkmiec@gmail.com>
2024-04-21 18:23:54 -0500

    single-head attn to fix matx size mismatch

--- attn_graph_forward_sol.py
+++ attn_graph_forward_sol.py
@@ -8,18 +8,16 @@ class GACsol(torch.nn.Module):
     def __init__(self, in_channels, hidden_dim=64, embed_channels=32, aggr=PyG.aggr.SoftmaxAggregation(learn=True)):
         super().__init__()
         # message passing layers
-        self.conv1 = PyG.GATv2Conv(in_channels, 
+        self.conv1 = PyG.GATv2Conv((-1,-1), 
                                  hidden_dim, 
-                                 heads=16, 
                                  dropout=0.1,
-                                 add_self_loops=False, 
+                                 add_self_loops=False,
                                  aggr=aggr) 
         self.lin1 = PyG.Linear(-1, hidden_dim)
-        self.conv2 = PyG.GATConv(hidden_dim, 
-                                 embed_channels, 
-                                 heads=16,
+        self.conv2 = PyG.GATv2Conv((-1,-1), 
+                                 embed_channels,
                                  dropout=0.1, 
-                                 add_self_loops=False, 
+                                 add_self_loops=False,
                                  aggr=aggr)
         self.lin2 = PyG.Linear(-1, embed_channels)
         # pooling layer
@@ -30,12 +28,14 @@ class GACsol(torch.nn.Module):
     def forward(self, batch):
         x = batch.x
         edge_index = batch.edge_index
-        edge_attr = batch.edge_attr
+        #edge_attr = batch.edge_attr #treat as homogenous graph?
         # message passing
-        x = self.conv1(x, edge_index) #+ self.lin1(x)
-        x = x.relu()
-        #x = self.conv2(x, edge_index) #+ self.lin2(x)
-        x = x.relu()
+        x = self.conv1(x, edge_index) + self.lin1(x)
+        x = F.leaky_relu(x)
+        x = self.conv2(x, edge_index) + self.lin2(x)
+        x = F.leaky_relu(x)
         # pooling
-        x = self.pool3(x, edge_index, edge_attr).relu()
+        x = self.pool3(x, edge_index)
+        print(x)
+        x = F.tanh(x)
         return self.out(x) 
