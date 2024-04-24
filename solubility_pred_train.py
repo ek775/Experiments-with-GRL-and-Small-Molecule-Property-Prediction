4f98645266e7585f05162d0ddf35fe5123164573
ek775 <eliotkmiec@gmail.com>
2024-04-24 15:14:11 -0500

    debugged training script

--- solubility_pred_train.py
+++ solubility_pred_train.py
@@ -25,29 +25,26 @@ print(model.parameters)
 device = 'cpu' #stay on cpu for now...configuration issues #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 
-# inizialize the optimizer, loss fn
+# inizialize the optimizer
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, maximize=True)
-loss_fn = torch.nn.MSELoss()
-
 
 
 
 
 ### FUNCTIONS ###
 
-def train_loop(dataloader, model, optimizer, loss_fn):
+def train_loop(dataloader, model, optimizer):
+    """train the model"""
     # Set the model to training mode
     model.train()
     total_loss=0
     for batch in dataloader:
         optimizer.zero_grad()
         batch = batch.to(device)
-        print(batch) # debug me
         # Compute prediction and loss
         pred_log_sol = model(batch)
-        print(pred_log_sol) # debug me
         true_log_sol = batch.y
-        loss = loss_fn(pred_log_sol,true_log_sol)
+        loss = F.mse_loss(pred_log_sol,true_log_sol)
         # Backpropagation
         loss.backward()
         optimizer.step()
@@ -56,6 +53,7 @@ def train_loop(dataloader, model, optimizer, loss_fn):
     return float(loss)
 
 def validate(dataloader, model):
+    """compute validation metrics"""
     model.eval()
     with torch.no_grad():
         preds = []
@@ -63,7 +61,10 @@ def validate(dataloader, model):
         for i in dataloader:
             preds.append(model(i)[0])
             targets.append(i.y[0])
-        loss = torch.nn.MSELoss(preds,targets)
+        
+        preds = torch.Tensor(preds)
+        targets = torch.Tensor(targets)
+        loss = F.mse_loss(preds, targets) #validation loss
         return preds, targets, loss
 
 
@@ -75,25 +76,29 @@ def validate(dataloader, model):
 history = SummaryWriter('GATv2Conv')
 epochs = 100
 import time
+from statistics import fmean
 # begin training
 for e in range(1, epochs+1):
     print(f"=====|Epoch {e}|=====")
     start = time.time()
-    #train, backprop, validation_metrics
-    train_loss = train_loop(dataloader=train_dl, model=model, optimizer=optimizer, loss_fn=loss_fn)
-    val_loss, val_preds, val_targets = validate(val_dl, model)
-    val_error = [val_preds[i]-val_targets[i] for i in range(len(val_targets))]
+    #train, validation_metrics
+    train_loss = train_loop(dataloader=train_dl, 
+                            model=model, 
+                            optimizer=optimizer)
+    val_preds, val_targets, val_loss = validate(val_dl, model)
+    val_error = fmean([val_preds[i]-val_targets[i] for i in range(len(val_targets))])
     stop = time.time()
     # record training
-    history.add_scalars(tag='Training & Validation Loss', 
+    history.add_scalars(main_tag='Training & Validation Loss', 
                         tag_scalar_dict={'train':train_loss,
                                          'val':val_loss}, 
                         global_step=e)
-    history.add_scalars(tag='Raw Validation Error', 
-                        tag_scalar_dict={'pred_sol':val_preds, 
-                                         'target_sol':val_targets, 
-                                         'raw_error':val_error}, 
-                        global_step=e)
+    history.add_scalar(tag='Raw Validation Error', 
+                       scalar_value=val_error,
+                       global_step=e)
+    history.add_embedding(torch.stack((val_preds, val_targets)), 
+                          metadata=['val preds', 'val targets (log aq sol)'],
+                          global_step=e)
     # show progress in terminal
     print(f"train loss: {train_loss} | val loss: {val_loss} | raw error: {val_error} | Time: {stop-start}s")
 
