ac15a13e54b243e0802fc7ce927683b1b514e7a5
ek775 <eliotkmiec@gmail.com>
2024-04-25 16:18:09 -0500

    reduced padding

--- attn_graph_forward_sol.py
+++ attn_graph_forward_sol.py
@@ -25,14 +25,15 @@ class GACsol(torch.nn.Module):
         self.norm2 = PyG.norm.GraphNorm(embed_channels) # batch normalization
 
         # MLP and regression out
-        self.lin3 = PyG.Linear(-1, embed_channels)
+        #self.lin3 = PyG.Linear(-1, embed_channels)
         self.lin4 = PyG.Linear(-1, 1)
-        self.lin5 = PyG.Linear(-1, 1000)
+        self.lin5 = PyG.Linear(-1, 500)
         self.out = PyG.Linear(-1, 1)
 
     def forward(self, batch):
         x = batch.x
         edge_index = batch.edge_index
+
         # message passing
         x = self.conv1(x, edge_index) + self.lin1(x)
         x = F.leaky_relu(x)
@@ -41,13 +42,14 @@ class GACsol(torch.nn.Module):
         x = self.conv2(x, edge_index) + self.lin2(x)
         x = F.leaky_relu(x)
         x = self.norm2(x)
+
         # MLP
-        x = self.lin3(x)
-        x = F.relu(x)
+        #x = self.lin3(x)
+        #x = F.relu(x)
         x = self.lin4(x)
         x = F.relu(x)
         x = x.T[0] #transpose and remove empty dimension
-        x = F.pad(x, (0, (1000-len(x)))) # pad tensor with zeros, constant length
+        x = F.pad(x, (0, (500-len(x)))) # pad tensor with zeros, constant length
         x = self.lin5(x)
         x = F.logsigmoid(x)
         x = self.out(x) 
